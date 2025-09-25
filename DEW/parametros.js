5af755648b7893622141ee5c3e5dc413a2f1cd5f
toninavhd <tillidie9999@gmail.com>
2025-09-25 16:23:21 +0100

    repo updated

--- DEW/parametros.js
+++
@@ -1,8 +0,0 @@
-const param = process.argv;
-
-console.log("\n" + param[0]);
-console.log("\n" + param[1]);
-console.log("\n" + param[2]);
-console.log("\n" + param[3]);
-console.log("\n" + param[4]);
-
