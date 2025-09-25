332b7bbad8eacc01c72232a7116a93a46cff398c
toninavhd <tillidie9999@gmail.com>
2025-09-25 16:22:57 +0100

    repo updated

---
+++ DEW/UT1/x_ejemplos/apuntes/parametros.js
@@ -0,0 +1,8 @@
+const param = process.argv;
+
+console.log("\n" + param[0]);
+console.log("\n" + param[1]);
+console.log("\n" + param[2]);
+console.log("\n" + param[3]);
+console.log("\n" + param[4]);
+
