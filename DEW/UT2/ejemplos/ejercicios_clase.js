e555dbf5b492267f6d4dd58ba69ba3c3b932d90a
toninavhd <tillidie9999@gmail.com>
2025-10-07 18:39:54 +0100

    tarea cole

--- DEW/UT2/ejemplos/ejercicios_clase.js
+++ DEW/UT2/ejemplos/ejercicios_clase.js
@@ -1,2 +1,12 @@
 //Laboratorio de Drones Autónomos
 
+var drones = [
+{ id: 1, modelo: 'Golf', bateria:90 },
+{ id: 2, modelo: 'Futbol', bateria:80 },
+{ id: 3, modelo: 'Smurf', bateria:70 },
+]
+
+function addDrones() {
+    drones.push(dron)
+}
+
