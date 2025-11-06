262987bae04e353b4ff7181cd28bd91e052612e1
toninavhd <tillidie9999@gmail.com>
2025-11-06 15:10:46 +0000

    tarea

---
+++ DEW/UT2/repaso/ejercicios.js
@@ -0,0 +1,15 @@
+//Crea un array numeros = [3,7,2,9,5]. Recorre con for y muestra cada índice y valor en formato: i= <i> v=<valor>.
+
+
+//Suma todos los elementos de numeros con for y muestra el total.
+
+const numeros = [3,7,2,9,5]
+
+let suma = 0;
+for(let i=0;i<numeros.length;i++) {
+    suma = suma + numeros[i]   
+};
+console.log(suma);
+
+// Genera un nuevo array pares con los valores pares de numeros sin usar filter. Usa for y push.
+
