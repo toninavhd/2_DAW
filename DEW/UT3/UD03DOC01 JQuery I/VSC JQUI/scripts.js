3c6f8dc47b0c661393411f012d9214e37898c3a5
toninavhd <tillidie9999@gmail.com>
2025-11-19 17:13:56 +0000

    a

--- DEW/UT3/UD03DOC01 JQuery I/VSC JQUI/scripts.js
+++ DEW/UT3/UD03DOC01 JQuery I/VSC JQUI/scripts.js
@@ -1,2 +1,25 @@
 "use strict";
 
+const $j = jQuery.noConflict();
+
+function inicializaComponentes(){
+    initAcordion();
+    initTabs();
+    initDialog();
+}
+
+function initAcordion(){
+    $j("#accordion").accordion();
+}
+
+function initDialog(){
+    $j("#dialog").dialog();
+}
+
+function initTabs(){
+    $j("#tabs").tabs();
+}
+
+$j(function(){
+    inicializaComponentes();
+});
