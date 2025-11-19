76b463734e81bdbcc4dd6a8e86d6ef6653c06bed
toninavhd <tillidie9999@gmail.com>
2025-11-19 16:45:15 +0000

    a

--- DEW/UT3/UD03DOC01 JQuery I/jquery_I/scripts.js
+++ DEW/UT3/UD03DOC01 JQuery I/jquery_I/scripts.js
@@ -5,9 +5,11 @@ function resolverEjercicios() {
   // 4. Resalta todos los elementos que tengan el atributo data-personaje.
   // 5. Modifica el texto del personaje con data-personaje="Dipper" para indicar que está en la biblioteca.
   // 6. Cambia la fuente de todos los elementos <p> a Georgia.
+  $("p").css("font-family","Times New Roman");
   // 7. Cambia el color del texto a rojo de los elementos con el atributo data-enemigo.
   // 8. Resalta los elementos cuyo atributo data-enemigo tenga el valor "true".
   // 9. Aplica cursiva a los elementos con data-personaje distinto de "Mabel".
+  $("[data-personaje].not[data-personaje!='Mabel']").css("font-style","italic");
   // 10. Cambia el fondo de los elementos cuyo atributo data-lugar comience con "caba".
   // 11. Muestra (fadeIn) los elementos cuyo atributo data-rol termine en "final".
   // 12. Aplica un borde rojo grueso a los elementos cuyo atributo title contenga la palabra "villano".
