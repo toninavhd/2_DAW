45c35e81b8e53a6a13a5533a00a17366118a5de8
toninavhd <tillidie9999@gmail.com>
2025-12-11 14:44:41 +0000

    ec2g

--- DEW/UT5/pytoBaseVacio VUE TS Router Pinia Vitest/src/App.vue
+++ DEW/UT5/pytoBaseVacio VUE TS Router Pinia Vitest/src/App.vue
@@ -1,4 +1,5 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import ButtonAutoCount from './components/ButtonAutoCount.vue'</script>
 
 <template>
   <h1>You did it!</h1>
@@ -6,6 +7,7 @@
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation
   </p>
+  <ButtonAutoCount>
 </template>
 
 <style scoped></style>
---
+++ DEW/UT5/pytoBaseVacio VUE TS Router Pinia Vitest/src/components/ButtonAutoCount.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <p> Soy ButtonAutoCount </p>
+        <button @click='count++'> Pulsado {{ count }} veces.</button>
+    </div>
+
+
+
+</template>
+
+<script setup lang='ts'>
+import {ref} from 'vue'
+
+    let count = ref<number>(0)
+
+</script>
+
+<style scoped>
+</style>
